6041ca0ec302a460d3851ffd82e723fef42af2b4
Son Ha <hason61vn@gmail.com>
2016-07-07 20:37:04 +0700

    upload file len server

---
+++ number.php
@@ -0,0 +1,34 @@
+<?php
+//Cac kieu du lieu trong PHP 
+
+
+// Int 
+// Float
+// String
+// Boolean
+// NULL
+// Object
+// Array
+$a = 12;
+$age = 24;
+$b = 2.5;
+
+$number1 = 2;
+$number2 = 3;
+$number3 = '4a';
+$number4 = 'abc';
+// $str = "Toi la Hieu va $age tuoi"; // '' ''
+// $str2 = 'Toi la Hieu va $age tuoi'; // '' ''
+
+// $str3 = ' va dep trai';
+// echo "$str.$str3";
+
+echo $number1 + $number2."<br>";
+echo $number1 + $number3."<br>";
+echo $number1 + $number4."<br>";
+
+echo "a =".$a;
+
+$flag = false;
+
+?>
